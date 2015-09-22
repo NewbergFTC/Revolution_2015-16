ec1005c7b1844e8cbc1e036371fc6392f2cce164
Garrison Peacock <Garrison.peacock@gmail.com>
2015-09-21 20:02:12 -0700

    More project setup and a barebones test opmode

---
+++ FtcRobotController/src/main/java/us/newberg/revolution/TestOpMode.java
@@ -0,0 +1,24 @@
+package us.newberg.revolution;
+
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * revolution-2015-16
+ * Created by Garrison Peacock on 9/21/15.
+ * All rights reserved.
+ */
+public class TestOpMode extends OpMode
+{
+    @Override
+    public void init()
+    {
+        DbgLog.msg("Init!");
+    }
+
+    @Override
+    public void loop()
+    {
+        DbgLog.msg("Loop!");
+    }
+}
